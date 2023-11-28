82ebdfff87088f160fbe53cc55bd6eb978f41a54
O-Gerler <azurmendi5o@outlook.com>
2023-11-28 12:13:43 +0100

    commit

---
+++ app/Http/Middleware/Antes.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class Antes 
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::info('Este mensaje es antes');
+        return $next($request);
+    }
+}
